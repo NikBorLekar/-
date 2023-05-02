856e1be87cdcb829a9e0920aed57236d45d2ea32
nik <lekares@gmail.com>
2023-05-02 11:08:27 +0700

    Initial prog

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+//string[] array = { "1234", "1567", "-2", "computer science" };
+// string[] array = { "Russia", "Denmark", "Kazan"};
+int count = 0;
+int index = 0;
+string temp = string.Empty;
+
