91adf0aa5bf85e8ef4aed17679062f4ddae982ec
nik <lekares@gmail.com>
2023-05-02 11:21:04 +0700

    Добавил метод

--- Program.cs
+++ Program.cs
@@ -23,3 +23,29 @@ if (count == 0)
     Console.Write("[ ]");
     return;
 }
+string[] array1 = new string[count];
+index = 0;
+int indexNew = 0;
+
+while (index < array.Length)
+{
+    temp = array[index];
+    if (temp.Length <= 3)
+    {
+        array1[indexNew] = array[index];
+        indexNew++;
+    }
+    index++;
+}
+
+PrintTextArray(array1);
+
+void PrintTextArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
